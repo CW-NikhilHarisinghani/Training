ce8dcd98cc47587bbaf955b7671c8d82eba87d01
Nikhil Deepak Harisinghani <nikhil.harisinghani@aepl.com>
2025-06-13 11:18:40 +0530

    Clean Up

---
+++ CSharp/Csharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HelloWorld.Csharp.HelloWorld
+{
+    public class HelloWorld
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World! me nigalo!");
+        }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        HelloWorld.Csharp.HelloWorld.HelloWorld.Main(args);
+        Console.WriteLine("Hello, World! me nigalo!");
+    }
+}
