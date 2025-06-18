97ea8b9eab4096e9d16e1a52ebd0084a46b58a18
Nikhil Deepak Harisinghani <nikhil.harisinghani@aepl.com>
2025-06-18 14:43:39 +0530

    Remove wwwroot/lib from tracking

---
+++ MVC/ProjectFolder/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectFolder.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
